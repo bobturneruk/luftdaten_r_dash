eb1b4197ddea69443eb01980b187cada32413f0f
Bob Turner <bob.turner.uk@gmail.com>
2019-08-06 20:17:11 +0100

    now using curl

--- functions/safe_download_file.R
+++ functions/safe_download_file.R
@@ -1,6 +1,7 @@
 safe_download_file<-function(url,name,destination_folder){
+  require(curl)
   tryCatch(
-    data.frame(download_code=download.file(url,paste0(destination_folder,name))),
+    data.frame(download_code=curl_download(url,paste0(destination_folder,name))),
     error=function(c) data.frame(error=as.character(c))
   )
 }
